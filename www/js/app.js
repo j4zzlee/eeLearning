2c1889e9e627ae1a75576145a11d8434363c3d97
Hai Huy <haibhut@gmail.com>
2016-03-17 23:09:18 +0700

    Haibh

--- www/js/app.js
+++ www/js/app.js
@@ -10,7 +10,8 @@ angular.module('starter', [
   'starter.controllers',
   'starter.services',
   'st2forget.word-unscramble-game',
-  'st2forget.action-verbs-game'
+  'st2forget.action-verbs-game',
+  'ngDraggable'
 ])
 
 .run(function($ionicPlatform) {

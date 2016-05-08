855729d0f7479e84cc543d7aa67af44adfd5ad1e
bhh <haibhut@gmail.com>
2016-05-08 21:50:19 +0700

    Update unordered sentence

--- www/js/app.js
+++ www/js/app.js
@@ -12,7 +12,8 @@ angular.module('starter', [
   'starter.controllers',
   'starter.services',
   'st2forget.word-unscramble-game',
-  'st2forget.action-verbs-game'
+  'st2forget.action-verbs-game',
+  'haibh.unordered-sentence-game'
 ])
 
 .run(function($ionicPlatform) {
